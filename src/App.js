188acee3c89c0c5afba669021306c0d7b099892d
TheDitmasParkerNews <78692363+TheDitmasParkerNews@users.noreply.github.com>
2021-02-07 10:19:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Archives from './components/Archives';
 
+export default class App extends React.Componenet {
+	render() {
+		return (
+			<>
+				<Router>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							&nbsp;&nbsp;
+							<li>
+								<Link to="/about">About us</Link>
+							</li>
+							&nbsp;&nbsp;
+							<li>
+								<Link to="/archives">Archives</Link>
+							</li>
+						</ul>
+					</nav>
+					<hr />
+					<Switch>
+						<Route exact path="/">
+								<Home />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/archives">
+							<Archives />
+						</Route>
+					</Switch>
+				</Router>
+			</>
+		);
+	}
+}
